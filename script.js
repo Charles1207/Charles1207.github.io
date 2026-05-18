0d088d5f610c3c5cfaf9d5036dba3d4aa7721fe0
Charles1207 <114063953+Charles1207@users.noreply.github.com>
2026-05-18 19:50:09 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+const cards = document.querySelectorAll('.card');
+
+cards.forEach(card => {
+
+    card.addEventListener('mouseenter', () => {
+
+        card.style.boxShadow = '0 0 20px #3b82f6';
+
+    });
+
+    card.addEventListener('mouseleave', () => {
+
+        card.style.boxShadow = 'none';
+
+    });
+
+});
